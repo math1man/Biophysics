abff76a7785287b8d74248fe06e2d2f0588c093a
Ari Weiland <math1man@gmail.com>
2016-02-08 19:01:22 -0600

    With some final optimizations, the fast BF algorithm flies! Over twice as fast as the old version The FastBF code has been moved into the normal BF sampler, and the FastBF is now gone

--- src/com/ariweiland/biophysics/sampler/BruteForceSampler.java
+++ src/com/ariweiland/biophysics/sampler/BruteForceSampler.java
@@ -2,8 +2,7 @@ package com.ariweiland.biophysics.sampler;
 
 import com.ariweiland.biophysics.Direction;
 import com.ariweiland.biophysics.Point;
-import com.ariweiland.biophysics.lattice.Folding;
-import com.ariweiland.biophysics.lattice.Lattice;
+import com.ariweiland.biophysics.lattice.BacktrackLattice;
 import com.ariweiland.biophysics.peptide.Polypeptide;
 
 import java.util.Arrays;
@@ -21,39 +20,32 @@ public class BruteForceSampler extends Sampler {
         Map<Double, Double> counter = new HashMap<>();
         int[] state = new int[size]; // we won't actually use the 0 index
         Arrays.fill(state, -1);
-        Lattice first = new Lattice(dimension, size);
-        first.put(new Point(0, 0, 0), polypeptide.get(0));
-        first.put(new Point(1, 0, 0), polypeptide.get(1));
-        Folding[] foldings = new Folding[size];
-        foldings[1] = new Folding(first, new Point(1, 0, 0), 1, 0);
-        int foldIndex = 2; // the first residue is directionless, and the second one just determines symmetry
+        BacktrackLattice lattice = new BacktrackLattice(dimension, size);
+        lattice.put(new Point(0, 0, 0), polypeptide.get(0));
+        lattice.put(new Point(1, 0, 0), polypeptide.get(1));
         long count = 0;
-        while (foldIndex > 1) {
+        while (lattice.size() > 1) {
             // Change the direction of the currently specified residue
-            state[foldIndex]++;
-            // If we go over, reset that direction and decrement the foldIndex
-            // so that it will modify the previous residue direction
-            if (state[foldIndex] >= 2 * dimension) {
-                state[foldIndex] = -1;
-                foldIndex--;
+            int index = lattice.size();
+            state[index]++;
+            // If we go over, reset that direction and remove the last residue
+            if (state[index] >= 2 * dimension) {
+                state[index] = -1;
+                lattice.removeLast();
             } else {
-                Point next = foldings[foldIndex - 1].lastPoint.getAdjacent(Direction.values()[state[foldIndex]]);
-                Lattice lastLattice = foldings[foldIndex - 1].lattice;
+                Point next = lattice.getLastPoint().getAdjacent(Direction.values()[state[index]]);
                 // Check that the generated state is valid
-                if (!lastLattice.containsPoint(next)) {
-                    Lattice lattice = new Lattice(lastLattice);
-                    lattice.put(next, polypeptide.get(foldIndex));
-                    if (foldIndex < size - 1) {
-                        // If valid and there are still more residues to position, go to the next residue
-                        foldings[foldIndex] = new Folding(lattice, next, foldIndex, 0);
-                        foldIndex++;
-                    } else {
-                        // Otherwise, increment the counter, reset the current residues, and go back
+                if (!lattice.containsPoint(next)) {
+                    lattice.put(next, polypeptide.get(index));
+                    if (lattice.size() == size) {
+                        // Otherwise, increment the counter
                         double energy = lattice.getEnergy();
                         if (!counter.containsKey(energy)) {
                             counter.put(energy, 0.0);
                         }
                         counter.put(energy, 1 + counter.get(energy));
+                        // and remove the last residue
+                        lattice.removeLast();
                         count++;
                         if (count % 1000000 == 0) {
                             System.out.println((count / 1000000) + "M states counted");
